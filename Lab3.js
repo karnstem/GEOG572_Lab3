5a0cefcc36ff2531f6b5df8f83a434c0319048d4
karnstem <mackenzierosekarnstein@gmail.com>
2026-02-08 02:21:15 -0800

    Add files via upload

--- Lab3.js
+++ Lab3.js
@@ -56,7 +56,7 @@ function calcPropRadius(attValue) {
     //constant factor adjusts symbol sizes evenly
     var minRadius = 5;
     //Flannery Apperance Compensation formula
-    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius /100;
 
     return radius;
 };
@@ -90,8 +90,8 @@ function callback(response2){
             pointToLayer: function (feature,latlng) {
                 var attValue = Number(feature.properties[attribute]);
                 var geojsonMarkerOptions = {
-                    radius: (calcPropRadius(attValue)/3000),
-                    fillColor: "#00ff33",
+                    radius: calcPropRadius(attValue),
+                    fillColor: "#ae00ff",
                     color: "#000",
                     weight: 1,
                     opacity: 1,
