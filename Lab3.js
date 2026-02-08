c1b341da23568a8211ce29bb3fb81d702f9613ab
karnstem <mackenzierosekarnstein@gmail.com>
2026-02-07 21:23:46 -0800

    Add files via upload

--- Lab3.js
+++ Lab3.js
@@ -28,18 +28,67 @@ function callback(response2){
     //set properties of geojson marker
     var geojsonMarkerOptions = {
         radius: 8,
-        fillColor: "#ff7800",
+        fillColor: "#ff00e6",
         color: "#000",
         weight: 1,
         opacity: 1,
         fillOpacity: 0.8
     };
+    //define popup function
+    function citypopupfunction(feature,layer){
+        var popupContent = "";
+        if (feature.properties) {
+            //loop to add feature property names and values to html string
+            for (var property in feature.properties){
+                popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+            }
+            layer.bindPopup(popupContent);
+        };
+    };
+    
     //add geojson
     L.geoJSON(response2, {
         pointToLayer: function (feature,latlng) {
             return L.circleMarker(latlng, geojsonMarkerOptions);
-        }
+        onEachFeature: Citypopupfunction;
+        },
     }).addTo(map);
+    //add popup about lat long
+    function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+    }
+    map.on('click', onMapClick);
+
+    //add popup to each feature
+    function onEachFeature(feature, layer) {
+        //no property named popupContent; instead, create html string with all properties
+        var popupContent = "";
+        if (feature.properties) {
+            //loop to add feature property names and values to html string
+            for (var property in feature.properties){
+                popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+            }
+            layer.bindPopup(popupContent);
+        };
+};
+
+//function to retrieve the data and place it on the map
+function getData(map){
+    //load the data
+    fetch("data/MegaCities.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json, {
+                onEachFeature: onEachFeature
+            }).addTo(map);
+        })  
+};
 }
 
 window.onload = jsAjax();
